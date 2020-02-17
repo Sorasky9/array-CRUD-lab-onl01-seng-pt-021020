776844cbe079dd439dce2022439ba302cd8dd24f
Malcom Fetus <sora.vista@gmail.com>
2020-02-17 05:11:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+["Aston Martin", "BMW", "Toyota"]
+array << "Porshe"
 end
 
 def add_element_to_start_of_array(array, element)
