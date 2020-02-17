99022fd18dc7d4c8c1939ea4a5e78f527450cbc7
Malcom Fetus <sora.vista@gmail.com>
2020-02-17 05:20:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 cars =["Aston Martin", "BMW", "Toyota"]
-cars.unshift = "wow!"
+cars.unshift = "wow"
 end
 
 def remove_element_from_end_of_array(array)
